86a694e8eb635de20a07816c798115c1afbc0ce9
rajkamal2013 <rajkamal2013@gmail.com>
2023-02-24 18:28:34 +0530

    Automake Bazel CMake

--- CMake/CPP/autogen.sh
+++ CMake/CPP/autogen.sh
@@ -1,3 +1,15 @@
+rm -rf install-sh
+rm -rf missing
+rm -rf config.status
+rm -rf config.log
+rm -rf aclocal.m4
+rm Makefile.in
+rm configure
+rm -rf autoMake
+
+
 aclocal
 autoconf
 automake --add-missing 
+
+mkdir autoMake
