e9944d2bec22edce7472c5be6ff284a0b1497890
Raj Kamal <raj.kamal@broadcom.com>
2024-12-06 12:08:40 +0530

    BackUp

---
+++ Projects/spelengine/src/main/java/org/example/spelengine/SpelengineApplication.java
@@ -0,0 +1,15 @@
+package org.example.spelengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpelengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpelengineApplication.class, args);
+	}
+
+}
