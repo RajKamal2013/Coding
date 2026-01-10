e1fb7d94e60217e197bddf51dbf184a7da3a2c46
Raj Kamal <rajkamal2013@gmail.com>
2026-01-10 13:03:19 +0530

    Backtracking codes

---
+++ DsAndAlgo/Java/Coding/src/main/java/org/patterns/TreeNode.java
@@ -0,0 +1,15 @@
+package org.patterns.twoPointer;
+
+
+// Definiton of a binary tree node class
+class TreeNode<T> {
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
